51a91325e53e823f9d14e5b0adab32f313498663
Rishi Pomal <38519496+rishi07it417@users.noreply.github.com>
2024-08-25 23:52:10 +0530

    add param retrieve from param store
    add param retrieve from param store

---
+++ awsparam/src/main/java/com/example/awsparam/App.java
@@ -0,0 +1,56 @@
+package com.example.awsparam;
+
+import java.util.Map;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App  {
+	
+	 public static void main(String[] args) {
+	      /*  final String usage = """
+
+	                Usage:
+	                    <paraName>
+
+	                Where:
+	                    paraName - The name of the parameter.
+	                """;
+
+	        if (args.length != 1) {
+	            System.out.println(usage);
+	            System.exit(1);
+	        }*/
+
+	        String paraName = "first-store-param";
+	        Region region = Region.AP_SOUTH_1;
+	        SsmClient ssmClient = SsmClient.builder()
+	                .region(region)
+	                .build();
+
+	        getParaValue(ssmClient, paraName);
+	        ssmClient.close();
+	    }
+
+	    public static void getParaValue(SsmClient ssmClient, String paraName) {
+	        try {
+	            GetParameterRequest parameterRequest = GetParameterRequest.builder()
+	                    .name(paraName)
+	                    .build();
+
+	            GetParameterResponse parameterResponse = ssmClient.getParameter(parameterRequest);
+	            System.out.println("The parameter value is :::::::::: " + parameterResponse.parameter().value());
+
+	        } catch (Exception e) {
+	            System.err.println(e.getMessage());
+	            System.exit(1);
+	        }
+	    }
+}
